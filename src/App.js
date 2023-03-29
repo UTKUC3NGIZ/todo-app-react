1b7e04f225d6d59919f1b1ba9f595c3d8ff37c9f
Utku Cengiz <cekicengiz01@gmail.com>
2023-03-29 20:46:01 +0300

    todo list and header UI completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useReducer } from "react";
+import reducer from "./reducers/todoReducer";
+import "./assets/css/reset.css";
+import "./app.css";
+import deneme from "./assets/images/icon-moon.svg";
 function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    todos: [],
+    todo: "",
+  });
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_TODO",
+      todo: state.todo,
+    });
+  };
+
+  const onChange = (e) => {
+    dispatch({
+      type: "SET_TODO",
+      value: e.target.value,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="mainDiv">
+        <div className="titleSection">
+          <h1>TODO</h1>
+          <span>
+            <a href="">
+              <img src={deneme} alt="" />
+            </a>
+          </span>
+        </div>
+        <div className="todoSection">
+          <div className="addTodo">
+            <span className="addTodo__checkbox" type="checkbox"></span>
+
+            <input
+              className="addTodo__text"
+              type="text"
+              placeholder="Create a new todo..."
+            />
+          </div>
+          <div className="mainTodo">
+            <ul>
+              <li>
+                {" "}
+                <div className="todoInput">
+                  <span className="todoInput__checkbox" type="checkbox"></span>
+
+                  <input
+                    className="todoInput__text"
+                    type="text"
+                    placeholder="Create a new todo..."
+                  />
+                </div>
+              </li>
+              <li>
+                {" "}
+                <div className="todoInput">
+                  <span className="todoInput__checkbox" type="checkbox"></span>
+
+                  <input
+                    className="todoInput__text"
+                    type="text"
+                    placeholder="Create a new todo..."
+                  />
+                </div>
+              </li>
+              <li>
+                {" "}
+                <div className="todoInput">
+                  <span className="todoInput__checkbox" type="checkbox"></span>
+
+                  <input
+                    className="todoInput__text"
+                    type="text"
+                    placeholder="Create a new todo..."
+                  />
+                </div>
+              </li>
+              <li>
+                {" "}
+                <div className="todoInput">
+                  <span className="todoInput__checkbox" type="checkbox"></span>
+
+                  <input
+                    className="todoInput__text"
+                    type="text"
+                    placeholder="Create a new todo..."
+                  />
+                </div>
+              </li>
+              <li>
+                {" "}
+                <div className="todoInput">
+                  <span className="todoInput__checkbox" type="checkbox"></span>
+
+                  <input
+                    className="todoInput__text"
+                    type="text"
+                    placeholder="Create a new todo..."
+                  />
+                </div>
+              </li>
+              <li>
+                {" "}
+                <div className="todoInput">
+                  <span className="todoInput__checkbox" type="checkbox"></span>
+
+                  <input
+                    className="todoInput__text"
+                    type="text"
+                    placeholder="Complete Todo App on Frontend Mentor"
+                  />
+                </div>
+              </li>
+            </ul>
+            <div className="todoSetting">
+              <div>
+                <p>5 items left</p>
+                <div>
+                  <button>All</button>
+                  <button>Active</button>
+                  <button>Completed</button>
+                </div>
+                <button>Clear Completed</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <form onSubmit={submitHandle}>
+        <input type="text" value={state.todo} onChange={onChange} />
+        <button disabled={!state.todo}>Ekle</button>
+      </form>
+      <ul>
+        {state.todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul> */}
+    </>
   );
 }
 
---
+++ src/deneme2.js
@@ -0,0 +1,27 @@
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      throw new Error();
+  }
+}
+
+function Counter() {
+  const [count, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
+    </div>
+  );
+}
+
+
+export default Counter
